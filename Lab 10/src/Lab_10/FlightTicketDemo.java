4fb05d027a0bfb1f8867594658f4053b3a5556bb
sourabhyelluru <sourabhyelluru@gmail.comn>
2020-12-15 16:32:52 +0530

    Corrected Decorator pattern code

--- Lab 10/src/Lab_10/FlightTicketDemo.java
+++ Lab 10/src/Lab_10/FlightTicketDemo.java
@@ -1,7 +1,6 @@
 package Lab_10;
 interface Tier{
 	int getCost();
-	String toString();
 }
 class Economy implements Tier{
 	int cost ;
@@ -10,9 +9,6 @@ class Economy implements Tier{
 	}
 	public int getCost() {
 		return cost;
-	}
-	public String toString() {
-		return "Economy";
 	}
 }
 class PremiumEconomy implements Tier{
@@ -23,9 +19,6 @@ class PremiumEconomy implements Tier{
 	public int getCost() {
 		return cost;
 	}
-	public String toString() {
-		return "Premium Economy";
-	}
 }
 class Business implements Tier{
 	int cost ;
@@ -34,9 +27,6 @@ class Business implements Tier{
 	}
 	public int getCost() {
 		return cost;
-	}
-	public String toString() {
-		return "Business";
 	}
 }
 class FirstClass implements Tier{
@@ -47,67 +37,42 @@ class FirstClass implements Tier{
 	public int getCost() {
 		return cost;
 	}
-	public String toString() {
-		return "FirstClass";
-	}
 }
-class FlightTicket implements Tier{
-	String start;
-	String end;
+//The following classes also implement Tier as per decorator pattern
+class Wifi implements Tier{
 	int cost;
-	Tier tier;
-	
-	public FlightTicket(String start, String end, String tier){
-		this.start = start;
-		this.end = end;
-		if(tier.equalsIgnoreCase("Economy")) {
-			this.tier = new Economy();
-			this.cost = new Economy().getCost();
-		}
-		else if(tier.equalsIgnoreCase("Premium Economy")) {
-			this.tier = new PremiumEconomy();
-			this.cost = new PremiumEconomy().getCost();
-		}
-		else if(tier.equalsIgnoreCase("Business")) {
-			this.tier = new Business();
-			this.cost = new Business().getCost();
-		}
-		else if(tier.equalsIgnoreCase("First Class")) {
-			this.tier = new FirstClass();
-			this.cost = new FirstClass().getCost();
-		}
+	Tier ticket;
+	public Wifi(Tier tick) {
+		this.cost = tick.getCost()+ 10;
 	}
-	@Override
 	public int getCost() {
 		return this.cost;
 	}
 }
-class AddAmenities implements Tier{
+class LiveTV implements Tier{
 	int cost;
-	public AddAmenities(String amenity, Tier ticket) {
-		if(amenity.equalsIgnoreCase("wifi")) {
-			cost = ticket.getCost()+10;
-		}
-		else if(amenity.equalsIgnoreCase("LiveTV")) {
-			cost = ticket.getCost()+50;
-		}
-		else if(amenity.equalsIgnoreCase("Wine")) {
-			cost = ticket.getCost()+30;
-		}
+	public LiveTV(Tier tick) {
+		this.cost = tick.getCost()+ 40;
 	}
 	public int getCost() {
-		return cost;
+		return this.cost;
 	}
 }
-public class FlightTicketDemo {
+class Wine implements Tier{
+	int cost;
+	public Wine(Tier tick) {
+		this.cost = tick.getCost()+ 50;
+	}
+	public int getCost() {
+		return this.cost;
+	}
+}
+public class FlightTicketDemo{
 	public static void main(String[] args) {
-		/*FlightTicket ticket1 = new FlightTicket("Delhi","Toronto","Economy");
-		FlightTicket ticket2 = new FlightTicket("Delhi","Toronto","Premium Economy");
-		FlightTicket ticket3 = new FlightTicket("Delhi","Toronto","Business");
-		FlightTicket ticket4 = new FlightTicket("Delhi","Toronto","First Class");*/
-		
-		System.out.println(new AddAmenities("wifi",new AddAmenities("LiveTV",new AddAmenities("Wine",new FlightTicket("Delhi","Toronto","First Class")))).getCost());
-		//Output : 5590
-		
+		Tier t1 = new FirstClass();
+		t1 = new Wifi(t1);
+		t1 = new LiveTV(t1);
+		t1 = new Wine(t1);
+		System.out.println(t1.getCost());
 	}
 }
